39a0cb84a1ab4eed7d774c0b874673c1d4eea02c
8gaU8 <71952256+8gaU8@users.noreply.github.com>
2026-01-15 17:19:46 +0900

    add starship

--- init.zsh
+++ init.zsh
@@ -12,13 +12,13 @@ export COMPLETION_CACHE_DIR="${HOME}/.cache/zsh_completions"
 export ACTIVATE_CACHE_DIR="${HOME}/.cache/zsh_activations"
 export MAX_CACHE_AGE_SECONDS=86400 # 1 day
 
-# Set ZSH_CUSTOM (for compatibility. should be fixed later)
-#export ZSH_CUSTOM="$(dirname $0)/custom"
-
 fpath+="${COMPLETION_CACHE_DIR}"
 
-# Setup Sheldon
-export SHELDON_CONFIG_DIR="$(dirname $0)/sheldon"
+# Custom Config Locations
+export BASE_CONFIG_DIR="$(dirname $0)/config"
+export SHELDON_CONFIG_FILE="${BASE_CONFIG_DIR}/sheldon-plugins.toml"
+export STARSHIP_CONFIG="${BASE_CONFIG_DIR}/starship.toml"
+
 eval "$(sheldon source)"
 
 autoload -Uz compinit && compinit
